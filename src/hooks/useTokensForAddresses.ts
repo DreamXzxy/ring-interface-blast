3ab6369f694c6a6427eaed3b8ab43dbde7bc0579
DreamXzxy <1784402100@qq.com>
2023-10-19 17:47:57 +0800

    Fix home page loading.

--- src/hooks/useTokensForAddresses.ts
+++ src/hooks/useTokensForAddresses.ts
@@ -2,21 +2,41 @@ import { addressesArray } from 'constants/tokens'
 import { Chain } from 'graphql/data/__generated__/types-and-hooks'
 import { InfoToken } from 'graphql/data/TopTokens'
 import { supportedChainIdFromGQLChain, unwrapToken } from 'graphql/data/util'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useReducer } from 'react'
 import { PoolData } from 'state/pools/reducer'
 
 import { useSearchTokens } from '../graphql/data/SearchTokens'
-import { CombinedData } from './InfoToken'
 
 interface UseInfoTokensReturnValue {
   infoTokens?: InfoToken[]
   loadingTokens: boolean
 }
 
+interface State {
+  loading: boolean;
+  infoTokens: InfoToken[] | undefined;  // Updated type here
+}
+
+interface Action {
+  type: string;
+  payload?: InfoToken[];
+}
+
 function useMultipleSearchTokens(addresses: string[], chainId?: number) {
   const tokensDataArray = addresses.map((address) => useSearchTokens(address, chainId ?? 1))
 
   return tokensDataArray
+}
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'SET_LOADING':
+            return { ...state, loading: true };
+        case 'SET_INFO_TOKENS':
+            return { ...state, loading: false, infoTokens: action.payload || undefined };  // Updated fallback value here
+        default:
+            throw new Error();
+    }
 }
 
 function useTokensForAddresses(addresses: string[], chainId?: number) {
@@ -45,64 +65,48 @@ function useTokensForAddresses(addresses: string[], chainId?: number) {
 }
 
 export function useInfoTokens(poolDatas: PoolData[], chain: Chain): UseInfoTokensReturnValue {
-  const combinedDataArray: CombinedData[] = []
-  const addressesToQuery = new Set<string>()
-  const chainId = supportedChainIdFromGQLChain(chain)
+  const [state, dispatch] = useReducer(reducer, { loading: true, infoTokens: undefined });
+  const chainId = supportedChainIdFromGQLChain(chain);
+  const { tokens: allTokens, loading: loadingTokens } = useTokensForAddresses(addressesArray, chainId);
+  const unwrappedTokens = allTokens.map((token: any) => unwrapToken(chainId ?? 1, token));
 
-  const { tokens: allTokens, loading, error } = useTokensForAddresses(addressesArray, chainId)
+  // Directly create tokensArray in useMemo
+  const tokensArray = useMemo(() => {
+    const tokensArr: InfoToken[] = [];
+    poolDatas.forEach((poolData) => {
+      const { token1, token0, tvlUSD, volumeUSD, volumeUSDWeek } = poolData;
+      const address0 = token0.address.toLowerCase();
+      const address1 = token1.address.toLowerCase();
+      
+      let addressKey = address1;
 
-  const unwrappedTokens = allTokens.map((token) => unwrapToken(chainId ?? 1, token))
-
-  poolDatas.forEach((poolData) => {
-    const { token1, token0, tvlUSD, volumeUSD, volumeUSDWeek } = poolData
-    const address0 = token0.address.toLowerCase()
-    const address1 = token1.address.toLowerCase()
-    const symbol0 = token0.symbol
-    const symbol1 = token1.symbol
-
-    let addressKey = address1
-
-    if (address1 === '0x3b94440c8c4f69d5c9f47bab9c5a93064df460f5'.toLowerCase()) {
-      addressKey = address0
-    }
-
-    addressesToQuery.add(addressKey)
-
-    combinedDataArray.push({
-      address: addressKey,
-      symbol0,
-      symbol1,
-      tvlUSD,
-      volumeUSD,
-      volumeUSDWeek,
-      tokenData: null, // Initialize tokenData as undefined
-    })
-  })
-
-  combinedDataArray.forEach((combinedData) => {
-    let addressKey = combinedData.address.toLowerCase()
-
-    // weth
-    if (addressKey === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') {
-      addressKey = 'NATIVE'
-    }
-    const matchingToken = unwrappedTokens.find((token) => token.address === addressKey)
-
-    if (matchingToken) {
-      // Update tokenData property with additional InfoToken properties
-      combinedData.tokenData = {
-        ...matchingToken,
-        tvlUSD: combinedData.tvlUSD,
-        volumeUSD: combinedData.volumeUSD,
-        volumeUSDWeek: combinedData.volumeUSDWeek,
+      if (address1 === '0x3b94440c8c4f69d5c9f47bab9c5a93064df460f5'.toLowerCase()) {
+        addressKey = address0;
       }
+
+      const matchingToken = unwrappedTokens.find((token: any) => token.address === addressKey);  // Consider providing a proper type for 'token' instead of 'any'
+
+      if (matchingToken) {
+        const infoToken = {
+          ...matchingToken,
+          tvlUSD,
+          volumeUSD,
+          volumeUSDWeek,
+        };
+        tokensArr.push(infoToken);
+      }
+    });
+    return tokensArr;
+  }, [poolDatas, unwrappedTokens]);
+
+  useEffect(() => {
+    if (!loadingTokens && tokensArray.length > 0) {
+      dispatch({ type: 'SET_INFO_TOKENS', payload: tokensArray });
     }
-  })
+  }, [loadingTokens, tokensArray]);
 
-  // Extract non-null and non-undefined tokenData values and return as InfoToken[]
-  const tokensArray: InfoToken[] = combinedDataArray
-    .map((combinedData) => combinedData.tokenData)
-    .filter((tokenData): tokenData is InfoToken => tokenData !== null && tokenData !== undefined)
-
-  return useMemo(() => ({ infoTokens: tokensArray, loadingTokens: loading }), [tokensArray, loading])
+  return {
+    infoTokens: state.loading ? undefined : state.infoTokens,
+    loadingTokens: state.loading,
+  };
 }
