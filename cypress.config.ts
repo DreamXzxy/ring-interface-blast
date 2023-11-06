b682abe616df8c5736be77e5e7bd4eb9cbc5f673
DreamXzxy <1784402100@qq.com>
2023-11-06 12:55:57 +0800

    fix: change cypress project id.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 import { setupHardhatEvents } from 'cypress-hardhat'
 
 export default defineConfig({
-  projectId: 'of36dc',
+  projectId: 'bt16j7',
   defaultCommandTimeout: 24000, // 2x average block time
   chromeWebSecurity: false,
   experimentalMemoryManagement: true, // better memory management, see https://github.com/cypress-io/cypress/pull/25462
